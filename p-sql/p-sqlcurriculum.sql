d09413afa9e9b974681c9ad55aa6dfae46537bce
11mrhappy <nekomamiremy@gmail.com>
2020-11-17 10:38:33 +0900

    Update p-sqlcurriculum.sql

--- p-sql/p-sqlcurriculum.sql
+++ p-sql/p-sqlcurriculum.sql
@@ -1,7 +1,7 @@
-SELECT *
-FROM users
-WHERE id NOT IN (
-    SELECT user_id
-    FROM shifts
-    WHERE date = "2015-07-01"
-)
+SELECT
+    CONCAT(last_name, first_name) "名前",
+    COUNT(*) "コマ数"
+FROM shifts s
+JOIN users u ON s.user_id = u.id
+WHERE date = "2015-07-01"
+GROUP BY user_id
