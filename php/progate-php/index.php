2dfa3ad723431680ff5cef8b9b40c51d82a7a521
11mrhappy <nekomamiremy@gmail.com>
2020-11-22 23:30:41 +0900

    switch php

--- php/progate-php/index.php
+++ php/progate-php/index.php
@@ -73,5 +73,25 @@
 ?>
 
 <?php
+  $num = 1;
+  $remainder = $num % 3;
+
+  switch($remainder){
+    case 0:
+      echo "大吉です。";
+      break;
+    case 1:
+      echo "中吉です。";
+      break;
+    case 2:
+      echo "小吉です。";
+      break;
+    default:
+      echo "凶です。";
+      break;
+  }
+?>
+
+<?php
   
 ?>
